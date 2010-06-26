fec0c4f57faed8a7ff690513ad9f95e7b8ca59a7
Michael Tanner <mike@thex90.org>
2010-06-26 14:19:31 -0400

    Unreal32 CHGHOST/IDENT and SETHOST/IDENT support for ConnectionNotice module

--- Rahu/Module/ConnectionNotice.pm
+++ Rahu/Module/ConnectionNotice.pm
@@ -3,9 +3,13 @@ package Rahu::Module::ConnectionNotice;
 use Rahu::Conf qw(rahu);
 use Rahu::Util qw(ircsend addhandler);
 
+addhandler("CHGHOST", "Rahu::Module::ConnectionNotice::chghostnotice");
+addhandler("CHGIDENT", "Rahu::Module::ConnectionNotice::chgidentnotice");
 addhandler("CONNECT", "Rahu::Module::ConnectionNotice::connnotice");
 addhandler("NICK", "Rahu::Module::ConnectionNotice::nicknotice");
 addhandler("QUIT", "Rahu::Module::ConnectionNotice::quitnotice");
+addhandler("SETHOST", "Rahu::Module::ConnectionNotice::sethostnotice");
+addhandler("SETIDENT", "Rahu::Module::ConnectionNotice::setidentnotice");
 
 sub connnotice {
 	my ($botnick, $target, $ident, $host, $gecos, $src) = @_;
@@ -20,6 +24,26 @@ sub nicknotice {
 sub quitnotice {
 	my ($botnick, $nick, $ident, $host, $gecos, $server, $msg) = @_;
 	ircsend(":$botnick PRIVMSG @{[rahu_conf_debugchan]} :Client quit: $nick ($ident\@$host) $gecos @ $server - $msg");
+}
+
+sub chghostnotice {
+	my ($botnick, $srcnick, $tgtnick, $ident, $host, $virthost) = @_;
+	ircsend(":$botnick PRIVMSG @{[rahu_conf_debugchan]} :Host change: $tgtnick ($ident\@$host => $ident\@$virthost) by $srcnick");
+}
+
+sub sethostnotice {
+	my ($botnick, $srcnick, $ident, $host, $virthost) = @_;
+	ircsend(":$botnick PRIVMSG @{[rahu_conf_debugchan]} :Host change: $srcnick ($ident\@$host => $ident\@$virthost)");
+}
+
+sub chgidentnotice {
+	my ($botnick, $srcnick, $tgtnick, $ident, $host, $newident) = @_;
+	ircsend(":$botnick PRIVMSG @{[rahu_conf_debugchan]} :Ident change: $tgtnick ($ident\@$host => $newident\@$host) by $srcnick");
+}
+
+sub setidentnotice {
+	my ($botnick, $srcnick, $ident, $host, $newident) = @_;
+	ircsend(":$botnick PRIVMSG @{[rahu_conf_debugchan]} :Ident change: $srcnick ($ident\@$host => $newident\@$host)");
 }
 
 1;
--- Rahu/Protocol/Unreal32.pm
+++ Rahu/Protocol/Unreal32.pm
@@ -9,16 +9,26 @@ sub init_link {
 	ircsend("PASS :@{[rahu_conf_linkpass]}");
 	ircsend("PROTOCTL VL");
 	ircsend("SERVER @{[rahu_conf_linkname]} 1 :U0-*-@{[rahu_conf_linkuuid]} @{[rahu_conf_linkdesc]}");
-	ircsend("NICK @{[rahu_conf_botnick]} 1 @{[time()]} @{[rahu_conf_botnick]} @{[rahu_conf_bothost]} @{[rahu_conf_linkname]} 0 +Sioq @{[rahu_conf_bothost]} :@{[rahu_conf_botgecos]}");
+	ircsend("NICK @{[rahu_conf_botnick]} 1 @{[time()]} @{[rahu_conf_botnick]} @{[rahu_conf_bothost]} @{[rahu_conf_linkname]} 0 +Siopq @{[rahu_conf_bothost]} :@{[rahu_conf_botgecos]}");
 	ircsend(":@{[rahu_conf_botnick]} JOIN @{[rahu_conf_debugchan]}");
-	ircsend(":@{[rahu_conf_botnick]} MODE @{[rahu_conf_debugchan]} +o @{[rahu_conf_botnick]} 0");
+	ircsend(":@{[rahu_conf_linkname]} MODE @{[rahu_conf_debugchan]} +o @{[rahu_conf_botnick]} 0");
 	in_debugchan(1);
 }
 
 sub irc_parse {
 	my ($caller, $msg) = @_;
 	debug("< $msg");
-	if ($msg =~ /^NICK (.*) (.*) (.*) (.*) (.*) (.*) (.*) :(.*)$/) {
+	if ($msg =~ /^:(\S+) CHGHOST (\S+) (\S+)$/) {
+		my ($src, $tgt, $host) = ($1, $2, $3);
+		handle_event("CHGHOST", rahu_conf_botnick, $src, $tgt, $user{$tgt}{ident}, $user{$tgt}{virthost}, $host);
+		$user{$tgt}{virthost} = $host;
+	}
+	elsif ($msg =~ /^:(\S+) CHGIDENT (\S+) (\S+)$/) {
+		my ($src, $tgt, $ident) = ($1, $2, $3);
+		handle_event("CHGIDENT", rahu_conf_botnick, $src, $tgt, $user{$tgt}{ident}, $user{$tgt}{virthost}, $ident);
+		$user{$tgt}{ident} = $ident;
+	}
+	elsif ($msg =~ /^NICK (.*) (.*) (.*) (.*) (.*) (.*) (.*) :(.*)$/) {
 		my ($nick, $hops, $ts, $ident, $host, $src, $noidea, $gecos) = ($1, $2, $3, $4, $5, $6, $7, $8, $9);
 		handle_event("CONNECT", rahu_conf_botnick, $nick, $ident, $host, $gecos, $src);
 		$user{$nick}{host} = $host;
@@ -32,17 +42,29 @@ sub irc_parse {
 		$user{$newnick} = $user{$oldnick};
 		delete($user{$oldnick});
 	}
-	elsif ($msg =~ /^PING :(.*)$/) {
-		ircsend("PONG $1");
-	}
 	elsif ($msg =~ /^:(\S+) NOTICE (.*) :(.*)$/) {
 		my ($src, $tgt, $msg) = ($1, $2, $3);
 		handle_event("NOTICE", $tgt, $src, $tgt, $msg);
+	}
+	elsif ($msg =~ /^PING :(.*)$/) {
+		ircsend("PONG $1");
 	}
 	elsif ($msg =~ /^:(\S+) QUIT :(.*)$/) {
 		my ($nick, $reason) = ($1, $2);
 		handle_event("QUIT", rahu_conf_botnick, $nick, $user{$nick}{ident}, $user{$nick}{host}, $user{$nick}{gecos}, $user{$nick}{server}, $reason);
 		delete $user{$nick};
+	}
+	elsif ($msg =~ /^:(\S+) SETHOST (\S+)$/) {
+		my ($nick, $host) = ($1, $2);
+		if (defined($user{$nick}{virthost})) {
+			handle_event("SETHOST", rahu_conf_botnick, $nick, $user{$nick}{ident}, $user{$nick}{host}, $host);
+		}
+		$user{$nick}{virthost} = $host;
+	}
+	elsif ($msg =~ /^:(\S+) SETIDENT (\S+)$/) {
+		my ($nick, $ident) = ($1, $2);
+		handle_event("SETIDENT", rahu_conf_botnick, $nick, $user{$nick}{ident}, $user{$nick}{virthost}, $ident);
+		$user{$nick}{ident} = $ident;
 	}
 	elsif ($msg =~ /:(.*) WHOIS (.*) :.*$/) {
 		ircsend(":@{[rahu_conf_linkname]} 311 $1 @{[rahu_conf_botnick]} @{[rahu_conf_botnick]} @{[rahu_conf_bothost]} * :@{[rahu_conf_botgecos]}");
